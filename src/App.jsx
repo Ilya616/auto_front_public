0235769594f7fc55030237a3c71b27e34c6a832f
Nikita <ntrofimov307@gmail.com>
2025-09-27 17:39:14 +0300

    Добавил страницу авторизации

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Registration from './components/pages/Auth/Registration'
+import { NavLink, Routes } from 'react-router'
+import FrontRoutes from './FrontRoutes'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+  <FrontRoutes />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,6 +32,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      
     </>
   )
 }
