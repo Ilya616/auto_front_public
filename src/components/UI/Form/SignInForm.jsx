fc191b5caa0a157ad44ed8edffb3b9bd77db7295
Nikita <ntrofimov307@gmail.com>
2025-09-28 21:55:32 +0300

    Перенёс кнопки и поля форм в UI

---
+++ src/components/UI/Form/SignInForm.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Button from '../Button/Button'
+import { NavLink } from 'react-router'
+import styles from '../../pages/Auth/Registration.module.scss'
+
+export default function Form() {
+  return (
+    <>
+        <h3 className={styles['form__title']}>Вход</h3>
+        <p><input type="text" className={styles['form__input']} placeholder="Логин" /></p>
+        <p><input type="password" className={styles['form__input']} placeholder="Пароль" /></p>
+        <p>
+          <Button 
+            className={styles['form__btn']} 
+          >
+            Войти
+          </Button>
+        </p>
+        <p><NavLink to={"/auth"} className={styles['form__forgot']}>Восстановить пароль</NavLink></p>
+    </>
+  )
+}
--- src/components/pages/Auth/Registration.jsx
+++ src/components/pages/Auth/Registration.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import styles from './Registration.module.scss';
 import { NavLink } from 'react-router'
+import Button from '../../UI/Button/Button';
+import SignInForm from '../../UI/Form/SignInForm';
+import SignUpForm from '../../UI/Form/SignUpForm';
 
 export default function Registration() {
   const [isActive, setIsActive] = useState(false);
@@ -18,45 +21,32 @@ export default function Registration() {
       <div className={styles.block}>
         <section className={`${styles['block__item']} ${styles['block-item']}`}>
           <h2 className={styles['block-item__title']}>У вас уже есть аккаунт ?</h2>
-          <button
-            onClick={handleSignIn}
-            className={`${styles['block-item__btn']} ${styles['signin-btn']}`}
+          <Button 
+            className={`${styles['block-item__btn']} ${styles['signin-btn']}`} 
+            event={handleSignIn}
           >
             Войти
-          </button>
+          </Button>
         </section>
         <section className={`${styles['block__item']} ${styles['block-item']}`}>
           <h2 className={styles['block-item__title']}>У вас нет аккаунта ?</h2>
-          <button
-            onClick={handleSignUp}
+          <Button 
             className={`${styles['block-item__btn']} ${styles['signup-btn']}`}
+            event={handleSignUp}
           >
             Зарегистрироваться
-          </button>
+          </Button>
         </section>
       </div>
 
       <div className={`${styles['form-box']} ${isActive ? styles.active : ''}`}>
         <form className={`${styles.form} ${styles['form_signin']}`}>
-          <h3 className={styles['form__title']}>Вход</h3>
-          <p><input type="text" className={styles['form__input']} placeholder="Логин" /></p>
-          <p><input type="password" className={styles['form__input']} placeholder="Пароль" /></p>
-          <p><button className={styles['form__btn']}>Войти</button></p>
-          <p><NavLink to={"/auth"} className={styles['form__forgot']}>Восстановить пароль</NavLink></p>
+          <SignInForm/>
         </form>
 
         <form className={`${styles.form} ${styles['form_signup']}`}>
-          <h3 className={styles['form__title']}>Регистрация</h3>
-          <p><input type="text" className={styles['form__input']} placeholder="Логин" /></p>
-          <p><input type="email" className={styles['form__input']} placeholder="Email" /></p>
-          <p><input type="password" className={styles['form__input']} placeholder="Пароль" /></p>
-          <p><input type="password" className={styles['form__input']} placeholder="Подтвердите пароль" /></p>
-          <p>
-            <button className={`${styles['form__btn']} ${styles['form__btn_signup']}`}>
-              Зарегистрироваться
-            </button>
-          </p>
-        </form>
+          <SignUpForm/>
+        </form>  
       </div>
     </article>
   );
