8825ec7a6b02ff8118e74dd6a66c7933ab18e4e3
PAVEL <studiovt@mail.ru>
2025-10-06 22:44:03 +0300

    loader and logout feature

--- src/components/pages/Lk/Lk.jsx
+++ src/components/pages/Lk/Lk.jsx
@@ -1,9 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { redirect, useLocation, useNavigate } from "react-router";
 import { request } from "../../Libs/request";
+import Loader from "@uiComponents/Loader/Loader";
+import styles from "./Lk.module.scss";
+import Button from "@uiComponents/Button/Button";
+
 let VITE_BACK_API = import.meta.env.VITE_BACK_API;
 
 export default function Lk() {
+  const [loader, setLoader] = useState(true);
   let navigete = useNavigate();
   useEffect(() => {
     if (!sessionStorage.getItem("token")) {
@@ -17,10 +22,32 @@ export default function Lk() {
           if (!response.data) {
             sessionStorage.removeItem("token");
             navigete("/auth");
+          } else {
+            setLoader(false);
           }
         },
       });
     }
   }, []);
-  return <div>Lk</div>;
+  function logout() {
+    sessionStorage.removeItem("token");
+    navigete("/auth");
+  }
+
+  return (
+    <div>
+      {loader ? (
+        <div className={styles.loader}>
+          <Loader />
+        </div>
+      ) : (
+        <>
+          <h1>lk</h1>
+          <Button event={logout} className={"button"}>
+            logout
+          </Button>
+        </>
+      )}
+    </div>
+  );
 }
