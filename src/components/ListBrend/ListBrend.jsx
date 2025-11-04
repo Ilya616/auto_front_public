36ae092653494d71c288cc57e93254b27bd9c88f
Ilya Poletuev <ilya.poletuev@kirarock.team>
2025-11-04 18:01:14 +0300

    вывод марок на главной

--- src/components/ListBrend/ListBrend.jsx
+++ src/components/ListBrend/ListBrend.jsx
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./ListBrend.module.scss";
 import Brend from "../UI/Components/Brend/Brend";
 import logo from "/Brends/audi.webp";
 import { BRENDS } from "../Libs/brends";
+import {request} from "../Libs/request";
+
+let VITE_BACK_API = import.meta.env.VITE_BACK_API;
+
 
 export default function ListBrend() {
+
+  let [marks , setmarks] = useState([])
+  useEffect(()=>{
+    request({
+      method: "get",
+      url: VITE_BACK_API + "/marks",
+      callback: (response) => {
+        setmarks(response.data.data);
+      },
+    });
+  },[]);
+  
+
+
   return (
     <div className={styles.brends}>
+
+      {marks.map((element) => (
+        <Brend
+          key={element.id}
+          img={element.img}
+          title={element.name}
+          length="1"
+        />
+      ))}
+
+
       {BRENDS.map((element) => (
         <Brend
           key={element.id}
